e47bb25ecb1dcc32b2a4d31bedec2109da683145
Wei Ren <weiren@DNa80ccd2.SUNet>
2021-03-15 13:41:45 -0700

    da+re

---
+++ robustqa_wr/eval_finetune_outdomain_da_re.sh
@@ -0,0 +1,5 @@
+python train_finetune_outdomain_da_re.py \
+--do-eval \
+--sub-file mtl_submission_val.csv \
+--save-dir save/"baseline_finetune_outdomain_da(best)+re" \
+--eval-dir datasets/oodomain_val
--- robustqa_wr/train.sh
+++
@@ -1,5 +0,0 @@
-python train.py \
---do-train \
---eval-every 2000 \
---reinit_layers 1 \
---run-name baseline
