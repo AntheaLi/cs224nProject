6805881eeb52be1335326d8f9ceac7b506452bf2
Wei Ren <weiren@DNa80ccd2.SUNet>
2021-03-15 19:58:52 -0700

    pure_reinit

---
+++ robustqa_wr/sh_reinit/eval_2.sh
@@ -0,0 +1,5 @@
+python train.py \
+--do-eval \
+--sub-file mtl_submission_val.csv \
+--save-dir save/baseline_with_reinit2-01 \
+--eval-dir datasets/oodomain_val
---
+++ robustqa_wr/sh_reinit/eval_3.sh
@@ -0,0 +1,5 @@
+python train.py \
+--do-eval \
+--sub-file mtl_submission_val.csv \
+--save-dir save/baseline_with_reinit3-01 \
+--eval-dir datasets/oodomain_val
